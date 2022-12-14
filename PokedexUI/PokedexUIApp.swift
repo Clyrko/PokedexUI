07bc00a6005015f168b16b14eeebd5f1943d7c05
Clyrko <justyn@effectstudios.co>
2022-12-14 21:00:25 +0000

    Initial Commit

---
+++ PokedexUI/PokedexUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexUIApp.swift
+//  PokedexUI
+//
+//  Created by Jay A. on 12/14/22.
+//
+
+import SwiftUI
+
+@main
+struct PokedexUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
